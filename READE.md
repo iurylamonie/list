e4ccdcc2bac6b6359cdedac07c51f1f456790485
Iury Lamonie <iury.escolar@hotmail.com>
2018-11-13 22:17:47 -0300

    Modificando funções

--- READE.md
+++ READE.md
@@ -1,8 +1,47 @@
-TAGS
+# Autores
 
-Duvida - Questão se pode usar assim
-Implemantar - funções comentadas que ainda vem ser implementadas
-O que retonar - Não sei o que retornar
-Temporario - Solução temporaria, ou seja, vai ser alterada futuramente.
+Iury Lamonie - Turma 2
+& 
+Leonardo Igor - Turma 1
 
-Alterar variaveis com o nome current
+# 1. Introdução:
+
+Este projeto contém a implementação de uma lista Abstract Data Type (ADT) baseada em uma lista duplamente encadeada.
+
+# 2. Makefile/Compiling:
+
+Verifica se o setup está configurado, caso contrario, fará um novo setup chamando "make dir" e compila. Ele chama "make all" internamente.
+
+```
+make
+```
+
+Você também pode passar argumentos para o compilador. Como o exemplo abaixo:
+```
+make args="-D something=212"
+```
+Para ativar o modo debug, faça:
+
+```
+make args="-D DEBUG"
+```
+
+Fará tods os diretórios de trabalho. Você pode fazer isso manualmente ou automaticamente se você usar "make".
+```
+make dir
+```
+
+Limpa todo o diretorio de trabalho, irá apagar todos os arquivos contidos em bin e docs
+```
+make clean
+```
+
+Fará a documentação para o projeto atual.
+```
+make doxy
+```
+
+Irá chamar "make clear" e "make dir"
+```
+make fresh
+```
--- include/list.h
+++ include/list.h
@@ -6,7 +6,7 @@
 
 using size_type = unsigned long;
 
-namespace ls
+namespace sc
 {
 	template < typename T >
 	class list
@@ -29,11 +29,10 @@ namespace ls
 		class const_iterator
 		{
 		public:
-			const_iterator(){ this->current = nullptr; }
-			const Node & operator*() const
-			{
-				return *this->current;
-			}
+			const_iterator( Node * p ) : current( p ) {}
+			
+			const Node & operator*() const { return *this->current; }
+
 			const_iterator & operator++()
 			{
 				this->current = this->current->next;
@@ -61,14 +60,13 @@ namespace ls
 
 		protected:
 			Node *current;
-			const_iterator( Node * p ) : current( p ) {}
 			friend class list< T >;
 		};
 
 		class iterator
 		{
 		public:
-			iterator() : const_iterator() { /* Empty */ };
+			iterator( Node *p ) : const_iterator( p ) { /* Empty */ };
 			const Node & operator*() const
 			{
 				return *this->current;
@@ -100,16 +98,25 @@ namespace ls
 				this->operator--();
 				return *temp;
 			}
+			bool operator==( const iterator & rhs ) const { return this->current == rhs->current; }
+			bool operator!=( const iterator & rhs ) const { return this->current == rhs->current; }
 
-		protected:
-			iterator( Node *p ) : const_iterator( p ) { /* Empty */ };
+		protected:			
 			friend class list<T>;
 		};
 
 		//== SPECIAL MEMBERs
 
+		/**
+		 * @brief      Default constructor that creates an empty list.
+		 */
 		list(){ this->iniciar_list(); }
 		
+		/**
+		 * @brief      Constructs the list with count default-inserted instances of T .
+		 *
+		 * @param[in]  the size of the list.
+		 */
 		explicit list( size_type count )
 		{		
 			this->iniciar_list();
@@ -119,28 +126,47 @@ namespace ls
 			}
 		}
 
+		/**
+		 * @brief      Constructs the list with the contents of the range [first, last) .
+		 *
+		 * @param[in]  first    The first element on the rage
+		 * @param[in]  last     The last element on the rage
+		 *
+		 * @tparam     InputIt  Inmput Iterator
+		 */
 		template < typename InputIt >
 		list( InputIt first, InputIt last )
 		{
 			this->iniciar_list();
-			InputIt current = first; //< Aponta para o valor atual
-			while( current != last )
+			InputIt temp = first; //< Aponta para o valor atual
+			while( temp != last )
 			{
-				this->push_back( *current ); //< Insere na ordem certa
+				this->push_back( *temp ); //< Insere na ordem certa
+				++temp;
 			}
 		}
 
+		/**
+		 * @brief      Copy constructor. Constructs the list with the deep copy of the contents of other .
+		 *
+		 * @param[in]  another list to be used as source to initialize the elements of the list with.
+		 */
 		list( const list & other )
 		{
 			this->iniciar_list();
-			Node * current = other.m_head; //< Aponta para o head da outra lista
+			Node * temp = other.m_head; //< Aponta para o head da outra lista
 			for( size_type i = 0; i <  other.m_size; ++i ) //< Percorre a outra lista 
 			{
-				this->push_back( current->data );
-				current = current->next; //< Aponta o current para o proximo nó.
+				this->push_back( temp->data );
+				temp = temp->next; //< Aponta o temp para o proximo nó.
 			}
 		}
 
+		/**
+		 * @brief      Replaces the contents with those identified by initializer list ilist .
+		 *
+		 * @param[in]  initializer list to initialize the elements of the list with.
+		 */
 		list( std::initializer_list<T> ilist )
 		{
 			this->iniciar_list();
@@ -150,20 +176,39 @@ namespace ls
 			}
 		}
 		
+		/**
+		 * @brief      Destructs the list. The destructors of the elements are called and the 
+		 * used storage is deallocated. Note, that if the elements are pointers, 
+		 * the pointed-to objects are not destroyed.
+		 */
 		~list(){ this->clear(); }
 		
+		/**
+		 * @brief      Copy assignment operator. Replaces the contents with a copy of the contents of other.
+		 *
+		 * @param[in]  another list to be used as source to initialize the elements of the list with.
+		 *
+		 * @return     The list
+		 */
 		list & operator=( const list & other )
 		{
 			this->iniciar_list();
-			Node * current = other.m_head; //< Aponta para o head da outra lista
+			Node * temp = other.m_head; //< Aponta para o head da outra lista
 			for( size_type i = 0; i <  other.m_size; ++i ) //< Percorre a outra lista 
 			{
-				this->push_back( current->data );
-				current = current->next; //< Aponta o current para o proximo nó.
+				this->push_back( temp->data );
+				temp = temp->next; //< Aponta o temp para o proximo nó.
 			}
 			return *this;
 		}
 
+		/**
+		 * @brief      Replaces the contents with those identified by initializer list ilist .
+		 *
+		 * @param[in]  initializer list to initialize the elements of the list with.
+		 *
+		 * @return     The list
+		 */
 		list & operator=( std::initializer_list<T> ilist )
 		{
 			this->iniciar_list();
@@ -175,57 +220,108 @@ namespace ls
 		}
 
 		//== DEBUG
-		void printList() //< Função para printar a list.
+
+		/**
+		 * @brief      Função para printar a list.
+		 */
+		void printList()
 		{
 			std::cout << "[ ";
-			Node * current = this->m_head;
+			Node * temp = this->m_head;
 			for( size_type i = 1; i <= this->m_size; ++i )
 			{
-				std::cout << current->data << " ";
-				current = current->next;
+				std::cout << temp->data << " ";
+				temp = temp->next;
 			}
 			std::cout << "] - Size: " << this->m_size << std::endl;
 		}
 
 		//== ITERATORS
-		iterator begin();
-		const_iterator cbegin() const;
-		iterator end();
-		const_iterator cend() const;
+
+		/**
+		 * @brief      returns an iterator pointing to the first item in the list
+		 *
+		 * @return     returns an iterator
+		 */
+		iterator begin() { return iterator( this->m_head ); }
+		
+		/**
+		 * @brief      returns an iterator pointing to the first item in the list
+		 *
+		 * @return     returns an iterator
+		 */
+		const_iterator cbegin() const { return const_iterator( this->m_head ); }
+		
+		/**
+		 * @brief      returns an iterator pointing to the end mark in the list
+		 *
+		 * @return     returns an iterator
+		 */
+		iterator end() { return ++(iterator( this->m_tail )); }
+		
+		/**
+		 * @brief      returns an iterator pointing to the end mark in the list
+		 *
+		 * @return     returns an iterator
+		 */
+		const_iterator cend() const { return ++(const_iterator( this->m_tail )); }
 
 		//== Capacity
 
+		/**
+		 * @brief      return the number of elements in the container.
+		 *
+		 * @return     m_size
+		 */
 		size_type size() const { return this->m_size; }
 
+		/**
+		 * @brief      returns true if the container contains no elements, and false otherwise.
+		 *
+		 * @return     true or false
+		 */
 		bool empty() const { return (this->m_size == 0); }
 
 		//== MODIFIERS
+
+		/**
+		 * @brief      remove all elements from the container.
+		 */
 		void clear(){ this->iniciar_list(); } 
 		
-		T & front() //< Alterar para trabalhar com begin 
-		{ 
-			/*if( !empty() )*/ return this->m_head->data; 
-			//return T(); //< O que retonar
-		}
+		/**
+		 * @brief      returns the object at the beginning of the list.
+		 *
+		 * @return     The element
+		 */
+		T & front() { return this->m_head->data; }
 		
-		const T & front() const //< Alterar para trabalhar com cbegin
-		{
-			/*if( !empty() )*/ return this->m_head->data; 
-			//return T(); //< O que retonar
-		}
+		/**
+		 * @brief      returns the object at the beginning of the list.
+		 *
+		 * @return    The element
+		 */
+		const T & front() const { return this->m_head->data; }
 		
-		T & back() //< Alterar para trabalhar com end
-		{
-			/*if( !empty() )*/ return this->m_tail->data; 
-			//return T(); //< O que retonar
-		}
+		/**
+		 * @brief      returns the object at the end of the list.
+		 *
+		 * @return     The element
+		 */
+		T & back() { return this->m_tail->data; }
 
-		const T & back() const //< Alterar para trabalhar com cend
-		{
-			/*if( !empty() )*/ return this->m_tail->data; 
-			//return T(); //< O que retonar
-		}
+		/**
+		 * @brief      returns the object at the end of the list.
+		 *
+		 * @return     The element
+		 */
+		const T & back() const { return this->m_tail->data; }
 		
+		/**
+		 * @brief      adds value to the front of the list.
+		 *
+		 * @param[in]  value  The value
+		 */
 		void push_front( const T & value )
 		{
 			Node * new_node = new Node( value, /*prev*/ nullptr, /*next*/ nullptr );
@@ -242,6 +338,11 @@ namespace ls
 			++this->m_size;
 		}
 
+		/**
+		 * @brief      adds value to the end of the list.
+		 *
+		 * @param[in]  value  The value
+		 */
 		void push_back( const T & value )
 		{
 			Node * new_node = new Node( value, /*prev*/ nullptr, /*next*/ nullptr );
@@ -258,6 +359,9 @@ namespace ls
 			++this->m_size;
 		}
 		
+		/**
+		 * @brief      removes the object at the front of the list.
+		 */
 		void pop_front()
 		{
 			if( !empty() )
@@ -270,6 +374,9 @@ namespace ls
 			}
 		}
 		
+		/**
+		 * @brief      removes the object at the end of the list.
+		 */
 		void pop_back()
 		{
 			if( !empty() )
@@ -282,26 +389,223 @@ namespace ls
 			}
 		}
 		
+		
+		/**
+		 * @brief      replaces the content of the list with copies of value value.
+		 *
+		 * @param[in]  value  The value
+		 */
 		void assign( const T & value )
 		{
 			if( !empty() )
 			{
-				for( size_type i = 1; i < this->m_size; ++i )
+				Node * temp = this->m_head;
+				for( size_type i = 1; i <= this->m_size; ++i )
 				{
-					/* empty */ //< Vai utilizar o insert;
+					temp->data = value;
+					temp = temp->next;
 				}
 			}
 		}
 
 		//== MODIFIERS WITH ITERATORS
-		template < class InItr >
-		void assign( InItr first, InItr last );
-		void assign( std::initializer_list<T> ilist );
-		iterator insert( const_iterator itr, const T & value );
-		iterator insert( const_iterator pos, std::initializer_list<T> ilist );
-		iterator erase( const_iterator itr );
-		iterator erase( const_iterator first, const_iterator last );
-		const_iterator find( const T & value ) const;
+
+		/**
+		 * @brief      dds value into the list before the position given by the iterator pos .
+		 *
+		 * @param[in]  pos    The position
+		 * @param[in]  value  The value
+		 *
+		 * @return     returns an iterator to the position of the inserted item.
+		 */
+		iterator insert( const_iterator pos, const T & value )
+		{
+			if( !empty() )
+			{
+				Node * pc = pos->current; 
+				Node * new_node = new Node( value, /*prev*/ pc->prev , /*next*/ pc );
+				if( pc->prev != nullptr) pc->prev->next = new_node; //< Faz o next do nó anterior apontar para novo nó
+				pc->prev = new_node;
+				++this->m_size;
+				return iterator( new_node );
+			}
+			
+			//Se a list estiver vazia
+			this->push_front( value ); 
+			return iterator( this->m_head );
+		}
+
+		/**
+		 * @brief      inserts elements from the range [first; last) before pos .
+		 *
+		 * @param[in]  pos    The position
+		 * @param[in]  first  The first
+		 * @param[in]  last   The last
+		 *
+		 * @tparam     InItr  Inmput Iterator
+		 *
+		 * @return     returns an iterator to the position of the inserted item first.
+		 */
+		template < typename InItr >
+		iterator insert( const_iterator pos, InItr first, InItr last )
+		{
+			size_type sz = last - first;
+			iterator * currIt = first; //< Iterador atual
+			iterator it; //< Aponta para o primeiro item inserido do rage na list
+			while( currIt != last )
+			{
+				if( currIt == first ) it = insert( pos, currIt->current->data );
+				else it( pos, currIt->current->data );
+				++currIt;
+			}
+
+			return it; //< Retornar o iterador para o primeiro elemento inserido do rage.
+		}
+
+		/**
+		 * @brief      nserts elements from the range [first; last) before pos .
+		 *
+		 * @param[in]  pos    The position
+		 * @param[in]  ilist  The ilist
+		 *
+		 * @return     returns an iterator to the position of the inserted item first.
+		 */
+		iterator insert( const_iterator pos, std::initializer_list<T> ilist )
+		{
+			iterator it; //< Aponta para o primeiro item inserido do ilist na list
+			size_type aux = 0;
+			for( auto i : ilist )
+			{
+				if( aux == 0 )
+				{
+					it = insert( pos, ilist );
+					++aux;	
+				}
+				else insert( pos, ilist );
+			}
+
+			return it;
+		}
+
+		iterator erase( const_iterator pos )
+		{
+			Node * pc = pos->current;
+			iterator it;
+			if( pc != nullptr )
+			{
+				if(pc->next != nullptr ) pc->next->prev = pc->prev; //< Verifica se é o ultimo nó
+				if(pc->prev != nullptr ) pc->prev->next = pc->next; //< verifica se é o primeiro nó
+				it = iterator( pc->next );
+			}
+			else it = iterator( pc );
+			pos->current = nullptr;
+			if(this->m_size > 0) --this->m_size;
+			return it;
+		}
+
+		iterator erase( const_iterator first, const_iterator last )
+		{
+			const_iterator it = first;
+			while( it != last )
+			{
+				erase(it);
+				++it;
+			}
+			if( it->current != nullptr )
+			{
+				erase( it );
+				++it;
+			} 
+			return it;
+		}
+
+		void assign( size_type count, const T& value )
+		{
+			erase( iterator(this->m_head), iterator(this->m_tail) ); //< Limpa a lista
+			for( size_type i = 0; i < count; ++i)
+			{
+				insert( iterator(m_tail), value );
+			}
+		}
+
+		template < typename InItr >
+		void assign( InItr first, InItr last )
+		{
+			erase( iterator(this->m_head), iterator(this->m_tail) ); //< Limpa a lista
+			InItr temp = first;
+			while( temp != last )
+			{
+				insert( iterator( this->m_tail, temp->current->data ) );
+				++temp;
+			}
+		}
+
+		void assign( std::initializer_list<T> ilist )
+		{
+			erase( iterator(this->m_head), iterator(this->m_tail) ); //< Limpa a lista
+			for( auto i: ilist )
+			{
+				insert( this->m_tail, i );
+			}
+		}
+		
+		const_iterator find( const T & value ) const
+		{
+			const_iterator it( this->m_head );
+			for( size_type i = 1; i <= this->m_size; ++i)
+			{
+				if( it->current->data == value ) return it;
+			}
+			return it;
+		}
+	
+		iterator find( const T & value )
+		{
+			iterator it( this->m_head );
+			for( size_type i = 1; i <= this->m_size; ++i)
+			{
+				if( it->current->data == value ) return it;
+				++it;
+			}
+			return it;
+		}
+
+		bool operator==( const list & rhs )
+		{
+			if(this->empty() && rhs.empty()) return true;
+			if( this->m_size == rhs->m_size )
+			{
+				iterator itLHS(this->m_head);
+				iterator itRHS(rhs->m_head);
+				for ( size_type  i = 1; i <= this->m_size; ++i)
+				{
+					if( itLHS->current->data != itRHS->current->data ) return false;
+					++itLHS;
+					++itRHS;
+				}
+			}
+
+			return true;
+		}
+
+		bool operator!=( const list & rhs )
+		{
+			if(this->empty() && rhs.empty()) return true;
+			if( this->m_size == rhs->m_size )
+			{
+				iterator itLHS(this->m_head);
+				iterator itRHS(rhs->m_head);
+				for ( size_type  i = 1; i <= this->m_size; ++i)
+				{
+					if( itLHS->current->data != itRHS->current->data ) return true;
+					++itLHS;
+					++itRHS;
+				}
+				return false;
+			}
+
+			return true;
+		}
 
 	private:
 		size_type m_size;
@@ -309,7 +613,11 @@ namespace ls
 		Node * m_tail;
 
 		//== Function
-		void iniciar_list() //< iniciar os atributos para usar os construtores
+
+		/**
+		 * @brief      iniciar os atributos para usar os construtores
+		 */
+		void iniciar_list()
 		{
 			this->m_head = nullptr;
 			this->m_tail = nullptr;
